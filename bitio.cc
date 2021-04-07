5384d22a042b0978a6867e4472162f0080d316ab
commonelk <commonelk@gmail.com>
2021-04-07 14:40:44 -0700

    Updated bitio constructor (Untested).

---
+++ bitio.cc
@@ -0,0 +1,16 @@
+#include <stdexcept>
+
+BitIO::BitIO(std::ostream* os, std::istream* is) {
+
+    // Throw errors if inputs are invalid
+    if(os == nullptr && is == nullptr) {
+        throw std::invalid_argument("os and is are both nullptr");
+    }
+    else if(os != nullptr && is == nullptr) {
+        throw std::invalid_argument("neither os nor is are nullptr")
+    }
+
+    // Otherwise, assign os and is values to class variables
+    os_ = os;
+    is_ = is;
+}
